f4a375cd052dfaee2d83c1d4f6f7e7fccd7fded2
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-15 23:36:01 -0500

    complete GET request and set up GalleryList component

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,4 +1,21 @@
 // New Component: GalleryList!
-// pass in the gallery data stored in `App` via `props`.
-// - Iterate (loop over) the list of gallery data
-// - Make GalleryItems
+
+// GOALS:
+// 1. pass in the gallery data stored in `App` via `props`
+// 2. iterate (loop over) the list of gallery data
+// 3. make GalleryItems
+// STEPS:
+// ✅ 1. create function in this file and give props as an argument. Props can be named anything.
+//    when I console.log props (or whatever I name it), it shows a big object
+// ✅ 2. in App.js import this GalleryList
+// 3. in App.js call this GalleryList by writing <GalleryList /> in the return
+//    where you want it to show up - ALSO NEED TO GIVE IT SOME PROPS!!!!!!!!!!!!!
+
+function GalleryList(props) {
+    console.log('props are', props);
+    return (
+        <p>THIS IS THE GALLERYLIST</p>
+    );
+}
+
+export default GalleryList;
