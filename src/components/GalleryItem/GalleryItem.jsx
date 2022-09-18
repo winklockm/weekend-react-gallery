9c6bcc4c9b0110779958e60c7fe9fd6c1ec3efff
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-17 19:09:52 -0500

    conditionally render description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,13 +2,22 @@ import { useState} from 'react';
 import axios from 'axios';
 
 // pass in the individual gallery item via `props`. 
-//     - Update the `GalleryList` to use this component to display an image.
-//     - Swap the image with the description on click. Use [conditional rendering](https://reactjs.org/docs/conditional-rendering.html).
-//     - Display the number likes for each item and include a like button.
-//     - When the like button is clicked, use `Axios` to update (`PUT`) the like count `/gallery/like/:id`.
-//     - Update the gallery each time a like button is clicked.
+//     ✅ - Update the `GalleryList` to use this component to display an image.
+//     ✅ - Swap the image with the description on click. Use [conditional rendering](https://reactjs.org/docs/conditional-rendering.html).
+//     ✅ - Display the number likes for each item and include a like button.
+//     ✅ - When the like button is clicked, use `Axios` to update (`PUT`) the like count `/gallery/like/:id`.
+//     ✅ - Update the gallery each time a like button is clicked.
 
 function GalleryItem({item, getData}) {
+
+// const for the description to be a boolean
+const [description, setDescription] = useState(false);
+
+// create const to be called by a click that will switch the boolean
+const showDescription = () => {
+    console.log('in showDescription');
+    setDescription(!description)
+   }
 
 const handleLike = (id) => {
     axios({
@@ -22,8 +31,8 @@ const handleLike = (id) => {
 }
 return (
     <div className="photoCard">
-        <img src={item.path} />
-        <p>Title: {item.title}</p>
+        {(description === false) ? <img src={item.path} onClick={showDescription}/> : 
+        <p onClick={showDescription}>{item.description}</p>}
         <button onClick={() => handleLike(item.id)}>LIKE!</button>
         <p>{item.likes} people like this</p>
     </div>
